4eefdae90dda64783936da8c24b257b6b3218b48
JoaquinGomez1 <joaquingomez3098@gmail.com>
2022-09-06 19:45:35 -0300

    fix: s3 uploads not working

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -1,14 +1,14 @@
 import aws from "aws-sdk";
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const s3BucketLocation = "us-east-1";
 
-const s3Bucket = new aws.S3({
+const s3 = new aws.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY || "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
   },
-  apiVersion: "v4",
+  signatureVersion: "v4",
   region: s3BucketLocation,
 });
 
@@ -17,20 +17,25 @@ const s3Bucket = new aws.S3({
  *  this only provides a secure url that will expire after certain amount of seconds
  *  so that an insecure app (i.e frontend) can use it to upload to s3 directly
  */
-async function genUploadUrl() {
-  const fileName = `${Math.random()}`.split(".")[1];
+async function genUploadUrl(fileType: string) {
+  const fileName = `${`${Math.random()}`.split(".")[1]}.png`;
 
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: fileName,
     Expires: parseInt(process.env.AWS_URL_EXPIRE_TIME!) || 60,
+    ContentType: "image/png",
   };
 
-  const url = await s3Bucket.getSignedUrlPromise("putObject", params);
+  const url = await s3.getSignedUrlPromise("putObject", params);
   return url;
 }
 
-export default async function handler(_: any, res: NextApiResponse) {
-  const url = await genUploadUrl();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const fileType = req.body.type || "image/png";
+  const url = await genUploadUrl(fileType);
   return res.status(200).json({ url });
 }
--- src/services/s3Images/uploadImage.ts
+++ src/services/s3Images/uploadImage.ts
@@ -1,9 +1,10 @@
 import { Image } from "@interfaces/image";
 import axios from "axios";
 
-export const uploadImageToS3 = (url: string, file?: Image) =>
-  axios.put(url, file, {
+export const uploadImageToS3 = (url: string, file?: Image) => {
+  return axios.put(url, file, {
     headers: {
-      "Content-Type": "multipart/form-data",
+      "Content-Type": "image/png",
     },
   });
+};
